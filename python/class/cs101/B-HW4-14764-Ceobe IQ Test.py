efd421645f4d93df6ad4bf9de6384d083f2226cc
zcjng <omegathunder564@gmail.com>
2025-10-17 13:46:14 +0800

    added class b hw4

---
+++ python/class/cs101/B-HW4-14764-Ceobe IQ Test.py
@@ -0,0 +1,41 @@
+m, n = map(int, input().split())
+
+board = [["."] * n for i in range(n)]
+
+col = set()
+posDiag = set() #this is (r+c)
+negDiag = set() #this is (r-c)
+
+result = 0
+def solve(rows, queens):
+    global result
+    
+    if queens > n - rows:
+        return 
+    if queens == 0:
+        result += 1
+        return
+    if rows == n:
+        return 
+    
+
+    for c in range(n):
+        if c in col or (rows + c) in posDiag or (rows - c) in negDiag:
+            continue
+        
+        col.add(c)
+        posDiag.add(rows + c)
+        negDiag.add(rows - c)
+
+        solve(rows + 1, queens - 1)
+        
+        col.remove(c)
+        posDiag.remove(rows + c)
+        negDiag.remove(rows - c)
+        
+    solve(rows + 1, queens)
+      
+
+        
+solve(0, m)
+print(result)
